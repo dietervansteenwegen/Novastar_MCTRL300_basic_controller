ded8eef285decee4088ea0f6ee4795f1eae463ac
Dieter Vansteenwegen <news1@vansteenwegen.org>
2022-04-15 14:14:00 +0200

    Works but missing cmd for WHITE_26

--- desay6_upad26_ctrl/desay6_upad26_LED.py
+++ desay6_upad26_ctrl/desay6_upad26_LED.py
@@ -41,28 +41,60 @@ class LedScreen:
 
 
 class Desay6(LedScreen):
-
-    NORMAL_6 = bytes.fromhex('23 FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 01 7C 58')
-    RED_6 = bytes.fromhex('2E FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 02 88 58')
-    GREEN_6 = bytes.fromhex('41 FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 03 9C 58')
-    BLUE_6 = bytes.fromhex('4B FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 04 A7 58')
-    WHITE_6 = bytes.fromhex('17 FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 05 74 58')
-    SLASH_6 = bytes.fromhex('4F FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 08 AF 58')
+    NORMAL_6 = bytes.fromhex('55 aa 00 23 FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 01 7C 58')
+    RED_6 = bytes.fromhex('55 aa 00 2E FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 02 88 58')
+    GREEN_6 = bytes.fromhex('55 aa 00 41 FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 03 9C 58')
+    BLUE_6 = bytes.fromhex('55 aa 00 4B FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 04 A7 58')
+    WHITE_6 = bytes.fromhex('55 aa 00 17 FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 05 74 58')
+    SLASH_6 = bytes.fromhex('55 aa 00 4F FE 00 01 00 FF FF 01 00 01 01 00 02 01 00 08 AF 58')
 
     def __init__(self, serport: str):
         super().__init__(serport)
+
+    def normal(self):
+        self.serport.write(self.NORMAL_6)
+
+    def red(self):
+        self.serport.write(self.RED_6)
+
+    def green(self):
+        self.serport.write(self.GREEN_6)
+
+    def blue(self):
+        self.serport.write(self.BLUE_6)
 
     def white(self):
         self.serport.write(self.WHITE_6)
 
+    def slash(self):
+        self.serport.write(self.SLASH_6)
+
 
 class Upad26(LedScreen):
-    NORMAL_26 = bytes.fromhex('67 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 01 C1 58')
-    RED_26 = bytes.fromhex('54 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 02 AF 58')
-    GREEN_26 = bytes.fromhex('74 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 03 D0 58')
-    BLUE_26 = bytes.fromhex('85 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 04 E2 58')
+    NORMAL_26 = bytes.fromhex('55 aa 00 67 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 01 C1 58')
+    RED_26 = bytes.fromhex('55 aa 00 54 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 02 AF 58')
+    GREEN_26 = bytes.fromhex('55 aa 00 74 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 03 D0 58')
+    BLUE_26 = bytes.fromhex('55 aa 00 85 FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 04 E2 58')
     # WHITE_26 = bytes.fromhex('55 aa 00 2b fe 00 00 00 00 00 00 00 02 00 00 00 02 00 82 56')
-    SLASH_26 = bytes.fromhex('8F FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 08 F0 58')
+    SLASH_26 = bytes.fromhex('55 aa 00 8F FE 00 01 01 FF FF 01 00 01 01 00 02 01 00 08 F0 58')
 
     def __init__(self, serport: str):
         super().__init__(serport)
+
+    def normal(self):
+        self.serport.write(self.NORMAL_26)
+
+    def red(self):
+        self.serport.write(self.RED_26)
+
+    def green(self):
+        self.serport.write(self.GREEN_26)
+
+    def blue(self):
+        self.serport.write(self.BLUE_26)
+
+    def white(self):
+        self.serport.write(self.WHITE_26)
+
+    def slash(self):
+        self.serport.write(self.SLASH_26)
