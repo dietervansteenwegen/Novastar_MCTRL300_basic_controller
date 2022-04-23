bea0291707c3b2515228e86f94500922b7252fb6
Dieter Vansteenwegen <github@vansteenwegen.org>
2022-04-23 22:19:09 +0200

    Implemented state machine for 3 stages.

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -6,7 +6,7 @@ import serial.serialutil
 from PyQt5 import QtWidgets
 from serports import serports
 
-from .main_window import Ui_MainWindow
+from .main_window_2 import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -19,16 +19,26 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.serport = None
         self.cmb_output.currentIndexChanged.connect(lambda index: self._output_changed(index))
         self.led_screen = None
+        self.state = 1
+        self._update_to_state()
+
+    def _change_state_to(self, state: int):
+        if state == 2 and self.cmb_output.currentIndex() > 0:
+            state = 3
+        self.state = state
+        self._update_to_state()
 
     def _output_changed(self, index: int):
-        # TODO: if serial port changes, self.led_screen does not get updated with the new port
-        # TODO: change so that first you need to select port, then output, then controls are shown
+
         if index == 0 or self.serport is None:
             self.led_screen = None
+            self._change_state_to(2)
         elif index == 1:
             self.led_screen = LED.Out1(self.serport)
+            self._change_state_to(3)
         elif index == 2:
             self.led_screen = LED.Out2(self.serport)
+            self._change_state_to(3)
 
     def _refresh_serial_ports(self) -> None:
         self.lst_serial_ports.clear()
@@ -43,12 +53,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.btn_serial_open.setEnabled(False)
             self.lbl_serial_status.setText('No ports found...')
             self.lbl_serial_status.setStyleSheet('background-color:orange')
+            self._change_state_to(1)
 
     def _open_serial_port(self, checked) -> None:
         if checked:
             if len(self.serial_available_ports) == 0:
                 # self.lbl_serial_status.setText('No serial ports')
                 self.btn_serial_open.setChecked(False)
+                self._change_state_to(1)
                 return
             index = self.lst_serial_ports.currentRow()
             try:
@@ -58,21 +70,37 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 print(e)
                 self._refresh_serial_ports()
                 self.btn_serial_open.setChecked(False)
+                self._change_state_to(1)
             if self.serport and self.serport.isOpen():
                 self.lbl_serial_status.setText(f'Opened {self.serial_available_ports[index][1]}')
                 self.btn_serial_open.setText(
                     f'Click to close {self.serial_available_ports[index][1]}', )
                 self.lbl_serial_status.setStyleSheet('background-color:green')
+                self._change_state_to(2)
             else:
                 self.lbl_serial_status.setText('Error opening port. See logs.')
                 self.lbl_serial_status.setStyleSheet('background-color:red')
                 self.serport = None
+                self._change_state_to(1)
         else:
             if self.serport:
                 self.serport.close()
             self.lbl_serial_status.setText('Closed serial port')
             self.lbl_serial_status.setStyleSheet('background-color:orange')
             self.btn_serial_open.setText('Click to open selected port')
+            self._change_state_to(1)
+
+    def _update_to_state(self):
+        self.cmb_output.setEnabled(self.state > 1)
+        self.sldr_brightness.setEnabled(self.state > 2)
+        self.btn_normal.setEnabled(self.state > 2)
+        self.btn_red.setEnabled(self.state > 2)
+        self.btn_green.setEnabled(self.state > 2)
+        self.btn_blue.setEnabled(self.state > 2)
+        self.btn_white.setEnabled(self.state > 2)
+        self.btn_slash.setEnabled(self.state > 2)
+        self.btn_freeze.setEnabled(self.state > 2)
+        self.btn_blackout.setEnabled(self.state > 2)
 
 
 def start_gui():
