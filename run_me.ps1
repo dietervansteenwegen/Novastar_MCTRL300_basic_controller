56f62f523d909fb82c1ff2a712c32b7a1b7bae2a
Dieter Vansteenwegen <github@vansteenwegen.org>
2022-04-24 18:03:45 +0200

    Don't need these in remote

--- run_me.ps1
+++
@@ -1,9 +0,0 @@
-Remove-Item -Recurse .git
-Remove-Item -Recurse .mypy_cache
-Remove-Item -Recurse .github
-Remove-Item -Recurse venv
-git init
-python -m venv venv
-mkdir docs
-cd docs
-sphinx-quickstart
